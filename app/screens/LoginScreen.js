24606f90c69dead945673b8d9adc064368e34974
Dvaid Adeleye <drizzzydave@gmail.com>
2021-07-25 19:48:46 +0100

    used Formik to handle form state

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,20 +1,29 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 import AppTextInput from "../components/AppTextInput"
 import AppButton from "../components/AppButton"
 import colors from "../config/colors";
-
+import {Formik} from 'formik'
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require("../assets/let-it-go.jpg")}/>
             <Text style={styles.text}>Let It Go!</Text>
-            <AppTextInput onChangeText={text => setEmail(text)} autoCapitalize="none" autoCorrect={false} icon="email" placeholder="Email" keyboardType="email-address" textContentType="emailAddress" />
-            <AppTextInput onChangeText={text => setPassword(text)}  autoCapitalize="none" autoCorrect={false} icon="lock" placeholder="Password" secureTextEntry textContentType="password" />
-            <AppButton title="Log In" onPress={()=> console.log(email, password)} color="black"/>
+            <Formik
+               initialValues = {{email: "", password: ""}}
+               onSubmit={values => console.log(values)}
+            > 
+                {({handleChange, handleSubmit})=> (
+            
+            <>
+                <AppTextInput onChangeText={handleChange("email")} autoCapitalize="none" autoCorrect={false} icon="email" placeholder="Email" keyboardType="email-address" textContentType="emailAddress" />
+                <AppTextInput onChangeText={handleChange("password")}  autoCapitalize="none" autoCorrect={false} icon="lock" placeholder="Password" secureTextEntry textContentType="password" />
+                <AppButton title="Log In" onPress={handleSubmit} color="black"/>
+            </>
+                )}
+            </Formik>
         </View>
     )
 }
