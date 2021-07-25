5e30c35fd42c5f9f13eede09cf70047c71233e59
Dvaid Adeleye <drizzzydave@gmail.com>
2021-07-25 18:50:29 +0100

    updated Login Screen and Button components

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
+import AppTextInput from "../components/AppTextInput"
+import AppButton from "../components/AppButton"
+import colors from "../config/colors";
+
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-        <View>
+        <View style={styles.container}>
             <Image style={styles.logo} source={require("../assets/let-it-go.jpg")}/>
-            <Text></Text>
+            <Text style={styles.text}>Let It Go!</Text>
+            <AppTextInput onChangeText={text => setEmail(text)} autoCapitalize="none" autoCorrect={false} icon="email" placeholder="Email" keyboardType="email-address" textContentType="emailAddress" />
+            <AppTextInput onChangeText={text => setPassword(text)}  autoCapitalize="none" autoCorrect={false} icon="lock" placeholder="Password" secureTextEntry textContentType="password" />
+            <AppButton title="Log In" onPress={()=> console.log(email, password)} color="black"/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 10
+    }, 
     logo: {
         width: 180, 
         height: 180, 
         alignSelf: "center", 
         marginTop: 60, 
-        marginBottom: 20
+       
+    }, 
+    text: {
+        fontSize: 17,
+         marginBottom: 30, 
+        alignSelf: "center",
+        fontStyle: "italic" 
+
     }
 })
 export default LoginScreen
