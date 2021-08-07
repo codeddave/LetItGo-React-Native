0505071ad4440231aeb483869d5c3988481b85c6
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-07 20:12:40 +0100

    styling updates in CategoryItemPicker component

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import styles from "../config/styles";
 import AppText from "./AppText";
 import Icon from "./Icon";
 
@@ -8,14 +7,20 @@ const CategoryPickerItem = ({ item, onPress }) => {
   return (
     <View style={styles.container}>
       <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-      <AppText>{item.label}</AppText>
+      <AppText style={styles.label}>{item.label}</AppText>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 30,
+    paddingHorizontal: 20,
     paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
   },
 });
 
