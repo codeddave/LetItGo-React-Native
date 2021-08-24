d0d85bdf2de0a79a4e2b34146ec8553cbac58ce3
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-24 21:07:26 +0100

    show error message for FormImagePicker component

--- app/components/FormImagePicker.js
+++ app/components/FormImagePicker.js
@@ -1,27 +1,31 @@
 import React from "react";
 import ImageInputList from "./ImageInputList";
 import { useFormikContext } from "formik";
+import ErrorMessage from "../components/ErrorMessage";
 
 const FormImagePicker = ({ name }) => {
   const { setFieldValue, touched, errors, values } = useFormikContext();
-
+  const imageUris = values[name];
   const handleAddImage = (uri) => {
-    setFieldValue(name, [...values[name], uri]);
+    setFieldValue(name, [...imageUris, uri]);
   };
 
   const handleRemoveImage = (uri) => {
     setFieldValue(
       name,
-      values[name].filer((imageUri) => imageUri !== uri)
+      imageUris.filer((imageUri) => imageUri !== uri)
     );
   };
 
   return (
-    <ImageInputList
-      imageUris={values[name]}
-      onRemoveImage={handleRemoveImage}
-      onAddImage={handleAddImage}
-    />
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onRemoveImage={handleRemoveImage}
+        onAddImage={handleAddImage}
+      />
+      {touched[name] ? <ErrorMessage error={errors[name]} /> : null}
+    </>
   );
 };
 
