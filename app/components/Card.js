2422c1930882dd10f22f69d5d6352319a1366423
David Adeleye <david@Davids-MacBook-Air.local>
2021-08-29 23:58:13 +0100

    handled navigation from ListingDetails screen

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,44 +1,46 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native'
 import colors from "../config/colors";
 
-export default function Card({title, subTitle, image}) {
+export default function Card({ title, subTitle, image, onPress }) {
     return (
-        <View style={styles.card}>
-            <Image style={styles.image} source ={image}/>
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View style={styles.card}>
+                <Image style={styles.image} source={image} />
 
-            <View style={styles.content}>
-                <Text style={styles.title}> {title} </Text> 
-                <Text style={styles.subTitle}> {subTitle} </Text>             
-             </View>
-        </View>
+                <View style={styles.content}>
+                    <Text style={styles.title}> {title} </Text>
+                    <Text style={styles.subTitle}> {subTitle} </Text>
+                </View>
+            </View>
+        </TouchableWithoutFeedback>
     )
 }
 
 const styles = StyleSheet.create({
     card: {
         height: 300,
-        width: "100%", 
-        borderRadius: 20, 
-        backgroundColor: colors.white, 
+        width: "100%",
+        borderRadius: 20,
+        backgroundColor: colors.white,
         overflow: "hidden",
-        marginBottom: 15, 
+        marginBottom: 15,
         marginTop: 10
 
-    }, 
+    },
     content: {
         padding: 10
     },
     image: {
         width: "100%",
-        height: 200, 
+        height: 200,
 
-    }, 
+    },
     subTitle: {
         fontWeight: "bold"
-    }, 
+    },
 
-    title:{
-        marginBottom:6
+    title: {
+        marginBottom: 6
     }
 })
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -5,50 +5,47 @@ import Card from "../components/Card"
 import colors from "../config/colors";
 
 
-
-
-
 const listings = [
     {
-        id: 1, 
-        title: 'Red jacket for sale', 
-        price: 100, 
+        id: 1,
+        title: 'Red jacket for sale',
+        price: 100,
         image: require("../assets/chair.jpg")
-        
-    }, 
-        {
-        id: 2, 
-        title: 'Couch in perfect condition', 
-        price: 1000, 
-        image: require("../assets/chair.jpg")
-        
-    }, 
-       /* {
-        id: 3, 
-        title: 'Couch in perfect condition', 
-        price: 1000, 
-        image: require("../assets/chair.jpg")
-        
-    }, 
-       {
-        id: 4, 
-        title: 'Couch in perfect condition', 
-        price: 1000, 
-        image: require("../assets/chair.jpg")
-        
-    } */
-]
- function ListingsScreen() {
-  return (
-    <View style={styles.screen}>
-      <FlatList 
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({item})=>  <Card title={item.title} subTitle={"$" + item.price} image={item.image}/> }
 
-      />
-     </View>
-  );
+    },
+    {
+        id: 2,
+        title: 'Couch in perfect condition',
+        price: 1000,
+        image: require("../assets/chair.jpg")
+
+    },
+    /* {
+     id: 3, 
+     title: 'Couch in perfect condition', 
+     price: 1000, 
+     image: require("../assets/chair.jpg")
+     
+ }, 
+    {
+     id: 4, 
+     title: 'Couch in perfect condition', 
+     price: 1000, 
+     image: require("../assets/chair.jpg")
+     
+ } */
+]
+function ListingsScreen({ navigation }) {
+    return (
+        <View style={styles.screen}>
+            <FlatList
+                data={listings}
+                keyExtractor={(listing) => listing.id.toString()}
+                renderItem={({ item }) => <Card title={item.title} subTitle={"$" + item.price} image={item.image} onPress={() => navigation.navigate("ListingsDetails")} />}
+
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
