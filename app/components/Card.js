82c9bd16f7b39397ea1c47f332a8a24d5919fc2e
David Adeleye <david@Davids-MacBook-Air.local>
2021-09-11 15:04:59 +0100

    updated Card component

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,46 +1,51 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
 import colors from "../config/colors";
 
 export default function Card({ title, subTitle, image, onPress }) {
-    return (
-        <TouchableWithoutFeedback onPress={onPress}>
-            <View style={styles.card}>
-                <Image style={styles.image} source={image} />
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.card}>
+        <Image style={styles.image} source={{ uri: image }} />
 
-                <View style={styles.content}>
-                    <Text style={styles.title}> {title} </Text>
-                    <Text style={styles.subTitle}> {subTitle} </Text>
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
-    )
+        <View style={styles.content}>
+          <Text style={styles.title}> {title} </Text>
+          <Text style={styles.subTitle}> {subTitle} </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        height: 300,
-        width: "100%",
-        borderRadius: 20,
-        backgroundColor: colors.white,
-        overflow: "hidden",
-        marginBottom: 15,
-        marginTop: 10
+  card: {
+    height: 300,
+    width: "100%",
+    borderRadius: 20,
+    backgroundColor: colors.white,
+    overflow: "hidden",
+    marginBottom: 15,
+    marginTop: 10,
+  },
+  content: {
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    resizeMode: "contain",
+  },
+  subTitle: {
+    fontWeight: "bold",
+  },
 
-    },
-    content: {
-        padding: 10
-    },
-    image: {
-        width: "100%",
-        height: 200,
-
-    },
-    subTitle: {
-        fontWeight: "bold"
-    },
-
-    title: {
-        marginBottom: 6
-    }
-})
+  title: {
+    marginBottom: 6,
+  },
+});
