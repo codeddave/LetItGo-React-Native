c6380ed22fc41693786703420b489a462f5faef6
David <drizzzydave@gmail.com>
2021-10-01 13:27:10 +0100

    react-native-progress for a Progress Bar component

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, Modal } from "react-native";
+import * as Progress from "react-native-progress";
+import colors from "../config/colors";
 
 const UploadScreen = ({ progress = 0, visible = false }) => {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Text>{progress * 100}</Text>
+        <Progress.Bar progress={progress} color={colors.primary} width={200} />
       </View>
     </Modal>
   );
