36183cc8b2524c1614dd837bb9e8d2ebd2312efd
David <drizzzydave@gmail.com>
2021-10-19 01:14:42 +0100

    handled getting item from Asnyc Storage

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,6 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import moment from "moment";
 
 const prefix = "cache";
+const expiryInMinutes = 5;
 
 const asyncStorageStore = async (key, value) => {
   try {
@@ -14,6 +16,30 @@ const asyncStorageStore = async (key, value) => {
   }
 };
 
+const isExpired = (item) => {
+  const now = moment(Date.now());
+  const storedTime = moment(item.timestamp);
+  return now.diff(storedTime, "minutes") > expiryInMinute;
+};
+const get = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    const item = JSON.parse(value);
+
+    if (!item) return null;
+
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+
+    return item.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default {
   asyncStorageStore,
+  get,
 };
