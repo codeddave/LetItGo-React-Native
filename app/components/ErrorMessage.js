508ba8f674e0fc23ab9a643c264b27aac03d9649
David <drizzzydave@gmail.com>
2021-11-04 12:53:23 +0100

    updates to the ErrorMessage component

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import AppText from "./AppText"
+import React from "react";
+import { StyleSheet } from "react-native";
+import AppText from "./AppText";
 
-
-const ErrorMessage = ({error}) => {
-    if(!error) return null
-    return (
-            <AppText style={styles.error}>{error}</AppText>
-    )
-}
+const ErrorMessage = ({ error }) => {
+  if (!error) return null;
+  return <AppText style={styles.error}>{error}</AppText>;
+};
 
 const styles = StyleSheet.create({
-    error: {
-        color: "red", 
-        padding: 5,
-    }
-})
-export default ErrorMessage
+  error: {
+    color: "red",
+    padding: 5,
+  },
+});
+export default ErrorMessage;
