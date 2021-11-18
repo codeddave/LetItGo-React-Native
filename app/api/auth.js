4d90c81f58f5b897c1d626766faf1d9e69076c05
codeddave <drizzzydave@gmail.com>
2021-11-18 21:19:40 +0100

    switching to custom backend

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,8 +1,11 @@
 import { create } from "apisauce";
-const apiClient = create({
+/* const apiClient = create({
   baseURL: "https://wander-lust-mern.herokuapp.com",
 });
-
+ */
+const apiClient = create({
+  baseURL: "http://localhost:5500",
+});
 export const logIn = async (loginData) => {
   return apiClient.post("/user/signin", loginData);
 };
