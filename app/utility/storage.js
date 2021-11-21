6fdd51670ebba6faf088e4ac6387fbf3f092b328
codeddave <drizzzydave@gmail.com>
2021-11-21 18:26:46 +0100

    fns to save and retrieve raw user token to SecureStore

--- app/utility/storage.js
+++ app/utility/storage.js
@@ -1,7 +1,24 @@
 import * as SecureStore from "expo-secure-store";
 
 const key = "userAuth";
+const tokenKey = "authToken";
 
+export const saveAuthTokenToStore = async (token) => {
+  try {
+    // const item = JSON.stringify(user);
+    await SecureStore.setItemAsync(tokenKey, token);
+  } catch (error) {
+    console.log("error getting auth token", error);
+  }
+};
+export const getAuthTokenFromStore = async () => {
+  try {
+    // console.log(await SecureStore.getItemAsync(tokenKey));
+    return await SecureStore.getItemAsync(tokenKey);
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const saveuserAuthToStore = async (user) => {
   try {
     const item = JSON.stringify(user);
