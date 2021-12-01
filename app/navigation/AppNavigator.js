1ed1c04e8f226ed1eea5afaa6a834e8496fe05aa
codeddave <drizzzydave@gmail.com>
2021-12-01 22:18:16 +0100

    request for push notification token

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -6,9 +6,21 @@ import AccountNavigator from "./AccountNavigator";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import NewListingButton from "./NewListingButton";
-
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 const Tab = createBottomTabNavigator();
 const AppNavigator = () => {
+  const registerForPushNotifications = () => {
+    try {
+      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+      if (!permission.granted) return;
+      const token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+    } catch (error) {
+      console.log("Error, getting a push token", error);
+    }
+  };
   return (
     <Tab.Navigator>
       <Tab.Screen
