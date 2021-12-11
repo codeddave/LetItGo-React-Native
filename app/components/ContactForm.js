8db3fd808f14f0ff357aabfeca13a080e66d5be1
codeddave <drizzzydave@gmail.com>
2021-12-11 22:15:58 +0100

    updated contact form, added contact seller button

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -2,11 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppForm from "./AppForm";
 import AppFormField from "./AppFormField";
-const ContactForm = () => {
+import SubmitButton from "./SubmitButton";
+const ContactForm = ({ listingCreator }) => {
   return (
     <View>
       <AppForm>
         <AppFormField />
+        <View>
+          <SubmitButton title="Contact Seller" />
+        </View>
       </AppForm>
     </View>
   );
