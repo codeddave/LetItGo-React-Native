1e7fcae31c77180475e0ef53105b850e211e9e23
codeddave <drizzzydave@gmail.com>
2021-12-11 13:54:07 +0100

    abstracted push notification logic into a usenotification hook

---
+++ app/components/hooks/useNotification.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState, useRef } from "react";
+import * as Notifications from "expo-notifications";
+
+import { sendPushNotificationToken } from "../../api/expoPushNotificationToken";
+
+import navigation from "../../navigation/rootNavigation";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+function useNotification(onNotificationRecieved, onNotificationClicked) {
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  const registerForPushNotifications = async () => {
+    let token;
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    sendPushNotificationToken(token, user?.email);
+    console.log(token);
+  };
+  useEffect(() => {
+    registerForPushNotifications();
+    if (onNotificationRecieved)
+      notificationListener.current =
+        Notifications.addNotificationReceivedListener(onNotificationRecieved);
+
+    if (onNotificationClicked)
+      responseListener.current =
+        Notifications.addNotificationResponseReceivedListener(
+          onNotificationClicked
+        );
+
+    return () => {
+      if (onNotificationRecieved)
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      if (onNotificationClicked)
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+}
+
+export default useNotification;
