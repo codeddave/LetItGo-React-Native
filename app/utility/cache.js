b52ae7101b25d3fc8d99c71476f6c8e3d51781fa
codeddave <drizzzydave@gmail.com>
2021-12-18 23:29:10 +0100

    switched out moment for dayjs

--- app/utility/cache.js
+++ app/utility/cache.js
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import moment from "moment";
+import dayjs from "dayjs";
+// import moment from "moment";
 
 const prefix = "cache";
 const expiryInMinutes = 5;
@@ -17,9 +18,11 @@ const asyncStorageStore = async (key, value) => {
 };
 
 const isExpired = (item) => {
-  const now = moment(Date.now());
-  const storedTime = moment(item.timestamp);
-  return now.diff(storedTime, "minutes") > expiryInMinutes;
+  //const now = moment(Date.now());
+  const now = dayjs();
+
+  const storedTime = dayjs(item.timestamp);
+  return now.diff(storedTime, "minute") > expiryInMinutes;
 };
 const get = async (key) => {
   try {
